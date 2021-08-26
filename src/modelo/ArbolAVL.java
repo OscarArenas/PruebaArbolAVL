7465c68555951cdc283a2f309c440d660cec9a93
Oscar Arenas <oharenas@gmail.com>
2021-08-25 19:59:02 -0500

    Operaciones básicas

---
+++ src/modelo/ArbolAVL.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright (C) 2020 Oscar Arenas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Oscar Arenas
+ */
+public class ArbolAVL {
+
+    private NodoBinarioAVL raiz;
+    private int n;
+
+    public ArbolAVL() {
+        raiz = null;
+        n = 0;
+    }
+
+    public boolean agregar(int dato) {
+        boolean inserto = insertar(dato);
+
+        if (inserto) {
+            balancear(dato);
+        }
+
+        return inserto;
+    }
+
+    // Inserta como en un ABB
+    private boolean insertar(int dato) {
+        NodoBinarioAVL actual = raiz;
+        NodoBinarioAVL padre = null;
+
+        while (actual != null) {
+            padre = actual;
+            if (dato < actual.dato) {
+                actual = actual.hijoIzquierdo;
+            } else if (dato > actual.dato) {
+                actual = actual.hijoDerecho;
+            } else {
+                return false;
+            }
+        }
+
+        NodoBinarioAVL nuevoNodo = new NodoBinarioAVL(dato);
+        if (padre == null) {
+            raiz = nuevoNodo;
+        } else if (dato < padre.dato) {
+            padre.hijoIzquierdo = nuevoNodo;
+        } else {
+            padre.hijoDerecho = nuevoNodo;
+        }
+
+        n++;
+        return true;
+    }
+
+    private void balancear(int dato) {
+        ArrayList<NodoBinarioAVL> ruta = camino(dato);
+
+        int k = ruta.size() - 1;
+
+        for (int i = k; i >= 0; i--) {
+            NodoBinarioAVL nodoA = ruta.get(i);
+            nodoA.actualizarAltura();
+
+            NodoBinarioAVL padreNodoA = (nodoA == raiz) ? null : ruta.get(i - 1);
+
+            switch (nodoA.factorBalance()) {
+                case -2:
+                    if (nodoA.hijoIzquierdo.factorBalance() <= 0) {
+                        rotacionLL(nodoA, padreNodoA);
+                    } else {
+                        rotacionLR(nodoA, padreNodoA);
+                    }
+                    break;
+                case 2:
+                    if (nodoA.hijoDerecho.factorBalance() >= 0) {
+                        rotacionRR(nodoA, padreNodoA);
+                    } else {
+                        rotacionRL(nodoA, padreNodoA);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void rotacionLL(NodoBinarioAVL nodoA, NodoBinarioAVL padreNodoA) {
+        NodoBinarioAVL nodoB = nodoA.hijoIzquierdo;
+
+        if (nodoA == raiz) {
+            raiz = nodoB;
+        } else if (padreNodoA.hijoIzquierdo == nodoA) {
+            padreNodoA.hijoIzquierdo = nodoB;
+        } else {
+            padreNodoA.hijoDerecho = nodoB;
+        }
+
+        nodoA.hijoIzquierdo = nodoB.hijoDerecho;
+        nodoB.hijoDerecho = nodoA;
+
+        nodoA.actualizarAltura();
+        nodoB.actualizarAltura();
+    }
+
+    private void rotacionLR(NodoBinarioAVL nodoA, NodoBinarioAVL padreNodoA) {
+        NodoBinarioAVL nodoB = nodoA.hijoIzquierdo;
+        NodoBinarioAVL nodoC = nodoB.hijoDerecho;
+
+        if (nodoA == raiz) {
+            raiz = nodoC;
+        } else if (padreNodoA.hijoIzquierdo == nodoA) {
+            padreNodoA.hijoIzquierdo = nodoC;
+        } else {
+            padreNodoA.hijoDerecho = nodoC;
+        }
+
+        nodoA.hijoIzquierdo = nodoC.hijoDerecho;
+        nodoB.hijoDerecho = nodoC.hijoIzquierdo;
+        nodoC.hijoIzquierdo = nodoB;
+        nodoC.hijoDerecho = nodoA;
+
+        nodoA.actualizarAltura();
+        nodoB.actualizarAltura();
+        nodoC.actualizarAltura();
+    }
+
+    private void rotacionRR(NodoBinarioAVL nodoA, NodoBinarioAVL padreNodoA) {
+        NodoBinarioAVL nodoB = nodoA.hijoDerecho;
+
+        if (nodoA == raiz) {
+            raiz = nodoB;
+        } else if (padreNodoA.hijoIzquierdo == nodoA) {
+            padreNodoA.hijoIzquierdo = nodoB;
+        } else {
+            padreNodoA.hijoDerecho = nodoB;
+        }
+
+        nodoA.hijoDerecho = nodoB.hijoIzquierdo;
+        nodoB.hijoIzquierdo = nodoA;
+
+        nodoA.actualizarAltura();
+        nodoB.actualizarAltura();
+    }
+
+    private void rotacionRL(NodoBinarioAVL nodoA, NodoBinarioAVL padreNodoA) {
+        NodoBinarioAVL nodoB = nodoA.hijoDerecho;
+        NodoBinarioAVL nodoC = nodoB.hijoIzquierdo;
+
+        if (nodoA == raiz) {
+            raiz = nodoC;
+        } else if (padreNodoA.hijoIzquierdo == nodoA) {
+            padreNodoA.hijoIzquierdo = nodoC;
+        } else {
+            padreNodoA.hijoDerecho = nodoC;
+        }
+
+        nodoA.hijoDerecho = nodoC.hijoIzquierdo;
+        nodoB.hijoIzquierdo = nodoC.hijoDerecho;
+        nodoC.hijoIzquierdo = nodoA;
+        nodoC.hijoDerecho = nodoB;
+
+        nodoA.actualizarAltura();
+        nodoB.actualizarAltura();
+        nodoC.actualizarAltura();
+    }
+
+    private ArrayList<NodoBinarioAVL> camino(int dato) {
+        ArrayList<NodoBinarioAVL> ruta = new ArrayList<>();
+
+        NodoBinarioAVL actual = raiz;
+
+        while (actual != null) {
+            ruta.add(actual);
+
+            if (dato < actual.dato) {
+                actual = actual.hijoIzquierdo;
+            } else if (dato > actual.dato) {
+                actual = actual.hijoDerecho;
+            } else {
+                break;
+            }
+        }
+        return ruta;
+    }
+
+    public boolean eliminar(int dato) {
+        NodoBinarioAVL actual = raiz;
+        NodoBinarioAVL padre = null;
+
+        while (actual != null) {
+            if (dato < actual.dato) {
+                padre = actual;
+                actual = actual.hijoIzquierdo;
+            } else if (dato > actual.dato) {
+                padre = actual;
+                actual = actual.hijoDerecho;
+            } else {
+                break;
+            }
+        }
+        if (actual == null) {
+            return false;
+        }
+
+        //Caso 1: el actual no tiene hijoIzquierdo
+        if (actual.hijoIzquierdo == null) {
+            if (padre == null) {
+                raiz = actual.hijoDerecho;
+            } else {
+                if (dato < padre.dato) {
+                    padre.hijoIzquierdo = actual.hijoDerecho;
+                } else {
+                    padre.hijoDerecho = actual.hijoDerecho;
+                }
+            }
+        } else {
+            // Caso 2: El actual tiene hijo izquierdo
+            NodoBinarioAVL padreMasALaDerecha = actual;
+            NodoBinarioAVL masALaDerecha = actual.hijoIzquierdo;
+
+            while (masALaDerecha.hijoDerecho != null) {
+                padreMasALaDerecha = masALaDerecha;
+                masALaDerecha = masALaDerecha.hijoDerecho;
+            }
+            actual.dato = masALaDerecha.dato;
+
+            if (padreMasALaDerecha.hijoDerecho == masALaDerecha) {
+                padreMasALaDerecha.hijoDerecho = masALaDerecha.hijoIzquierdo;
+            } else {
+                padreMasALaDerecha.hijoIzquierdo = masALaDerecha.hijoIzquierdo;
+            }
+        }
+        n--;
+        return true;
+    }
+
+    public int tamanio() {
+        return n;
+    }
+
+    public void imprimir() {
+        imprimir(raiz, "");
+    }
+
+    private void imprimir(NodoBinarioAVL r, String espacios) {
+        if (r != null) {
+            imprimir(r.hijoDerecho, espacios + "   ");
+            System.out.println(espacios + r.dato);
+            imprimir(r.hijoIzquierdo, espacios + "   ");
+        }
+    }
+}
---
+++ src/modelo/NodoBinarioAVL.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021 Oscar Arenas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package modelo;
+
+/**
+ *
+ * @author Oscar Arenas
+ */
+public class NodoBinarioAVL {
+
+    int dato;
+    NodoBinarioAVL hijoIzquierdo;
+    NodoBinarioAVL hijoDerecho;
+    int altura;
+
+    public NodoBinarioAVL(int dato) {
+        this.dato = dato;
+        hijoIzquierdo = null;
+        hijoDerecho = null;
+        altura = 1;
+    }
+
+    void actualizarAltura() {
+        if (esHoja()) {
+            altura = 1;
+        } else if (hijoIzquierdo == null) {
+            altura = hijoDerecho.altura + 1;
+        } else if (hijoDerecho == null) {
+            altura = hijoIzquierdo.altura + 1;
+        } else {
+            altura = Math.max(hijoIzquierdo.altura, hijoDerecho.altura) + 1;
+        }
+    }
+
+    int factorBalance() {
+        if (esHoja()) {
+            return 0;
+        } else if (hijoIzquierdo == null) {
+            return hijoDerecho.altura;
+        } else if (hijoDerecho == null) {
+            return -hijoIzquierdo.altura;
+        } else {
+            return hijoDerecho.altura - hijoIzquierdo.altura;
+        }
+    }
+
+    boolean esHoja() {
+        return hijoIzquierdo == null && hijoDerecho == null;
+    }
+}
